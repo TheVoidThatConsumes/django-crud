ce4f486e9c59372ef31f9ff1edc9a7ab6208b99d
TheVoidThatConsumes <lyricalluxor@gmail.com>
2022-06-24 14:09:11 +0200

    final commit

--- David/blog/admin.py
+++ David/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from blog.models import Post
 
 # Register your models here.
 
--- David/blog/views.py
+++ David/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from pyexpat import model
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from blog.models import Post
+from django.urls import reverse_lazy
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
